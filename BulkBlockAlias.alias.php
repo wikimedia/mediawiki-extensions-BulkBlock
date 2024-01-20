4691b6e0a660154d1773c11347a2b6ff416548d5
anterdc99 <xuminruixuan@live.cn>
2024-01-20 13:41:31 +0800

    BulkBlock: Add special page aliases for Chinese variants
    Change-Id: I1c0d5ebec39c980ecf34729044ad02276b6444c5

--- BulkBlockAlias.alias.php
+++ BulkBlockAlias.alias.php
@@ -12,3 +12,13 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'BulkBlock' => [ 'BulkBlock' ]
 ];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'BulkBlock' => [ '批量封禁' ]
+];
+
+/** Traditional Chinese (（中文（繁體））) */
+$specialPageAliases['zh-hant'] = [
+	'BulkBlock' => [ '批次封鎖' ]
+];
